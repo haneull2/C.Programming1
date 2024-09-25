b7e0bb6318f7a0cffc5cbe37827cce85978f83c6
haneull2 <kimha4005@naver.com>
2024-09-25 12:27:51 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int s = 1, n, i;
+		for (; s <= 9; s += 2) {
+		n = 4, i = 0;
+		for (; n > (s / 2); n--) {
+			printf(" ");
+		}
+		for (;i < s; i++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
