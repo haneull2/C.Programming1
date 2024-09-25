26496c97b4b3049c6d549e6ce24df8e046b9a649
haneull2 <kimha4005@naver.com>
2024-09-25 12:42:00 +0900

    줄바꿈 수정

--- hw3.c
+++ hw3.c
@@ -2,12 +2,12 @@
 
 int main(void) {
 	int s = 1, n, i;
-		for (; s <= 9; s += 2) {
+	for (; s <= 9; s += 2) {
 		n = 4, i = 0;
 		for (; n > (s / 2); n--) {
 			printf(" ");
 		}
-		for (;i < s; i++) {
+		for (; i < s; i++) {
 			printf("*");
 		}
 		printf("\n");
